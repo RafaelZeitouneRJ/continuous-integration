2747c9ca4e0f170b4ca812cdada52c8cecd95610
RafaelZeitouneRJ <rafaelzeitoune@yahoo.com.br>
2022-02-05 11:36:24 -0300

    Update após usar o eslint fix para corrigir

--- user-api/src/app.js
+++ user-api/src/app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const { MongoClient, ObjectId } = require("mongodb");
-const UserRepository = require("./user-repository");
-const bodyParser = require("body-parser");
+const express = require('express');
+const {MongoClient, ObjectId} = require('mongodb');
+const UserRepository = require('./user-repository');
+const bodyParser = require('body-parser');
 // const { ObjectId } = require('bson');
-const cors = require("cors");
+const cors = require('cors');
 // const { request } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
 // Coloquei esse Middle para fazer um teste
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+      extended: true,
+    }),
 );
 
 // Esse middle foi usado para permitir trabalhar com corcs
 app.use(
-  cors({
-    allowedHeaders: ["X-Total-Count", "Content-type"],
-    exposedHeaders: ["X-Total-Count", "Content-type"],
-  })
+    cors({
+      allowedHeaders: ['X-Total-Count', 'Content-type'],
+      exposedHeaders: ['X-Total-Count', 'Content-type'],
+    }),
 );
 
 let userRepository;
@@ -33,78 +33,78 @@ let connected = false;
 
 app.use(async (req, res, next) => {
   if (!connected) {
-    const uri = "mongodb://localhost";
+    const uri = 'mongodb://localhost';
     client = new MongoClient(uri);
     await client.connect();
-    const collection = client.db("users_db").collection("users");
+    const collection = client.db('users_db').collection('users');
     userRepository = new UserRepository(collection);
     connected = true;
   }
   next();
 });
 
-app.get("/users", async (request, response) => {
+app.get('/users', async (request, response) => {
   // Coloquei o TryCatch, pois no tratamento do findAll() estou gerando exceção para caso não ache nenhum valor
   try {
     const users = await userRepository.findAll();
-    response.setHeader("X-Total-Count", users.length); // Coloquei para atender os requisitos do react-admin
+    response.setHeader('X-Total-Count', users.length); // Coloquei para atender os requisitos do react-admin
     response.status(200).json(users);
   } catch (error) {
-    response.setHeader("X-Total-Count", 0); // Foi necessário colocar para permitir trabalhar com X-Total-count
+    response.setHeader('X-Total-Count', 0); // Foi necessário colocar para permitir trabalhar com X-Total-count
     response.status(200).json([]);
   }
 });
 
-app.post("/users", async (request, response) => {
+app.post('/users', async (request, response) => {
   // Coloquei o TryCatch, pois no tratamento do findAll() estou gerando exceção para caso não ache nenhum valor
   const user = await userRepository.insert(request.body);
   response.status(201).json(user);
 });
 
-app.get("/users/:id", async (request, response) => {
+app.get('/users/:id', async (request, response) => {
   try {
     const user = await userRepository.findOneById(ObjectId(request.params.id)); // params id vem pela url conforme o parametro que coloquei na rota
     // response.status(200).json(user); //Por padrão irá retornar o statusCode 200
     response.json(user);
   } catch (error) {
     response.status(404).json({
-      message: "User not found",
+      message: 'User not found',
       code: 404,
     });
   }
 });
 
 // O delete foi criado para esta aula e ainda não foi testado.
-app.delete("/users/:id", async (request, response) => {
+app.delete('/users/:id', async (request, response) => {
   try {
     await userRepository.delete(ObjectId(request.params.id));
     // response.status(200).json(request.body)
     response.status(200).json({
-      message: "User delete with success",
+      message: 'User delete with success',
       code: 200,
     });
   } catch (e) {
     // console.log(e.message)
     response.status(404).json({
-      message: "User not found",
+      message: 'User not found',
       code: 404,
     });
   }
 });
 
-app.put("/users/:id", async (request, response) => {
+app.put('/users/:id', async (request, response) => {
   try {
     // console.info(request.body);
     // console.info(ObjectId(request.params.id));
     const user = await userRepository.update(
-      ObjectId(request.params.id),
-      request.body
+        ObjectId(request.params.id),
+        request.body,
     ); // Ess é a versão que usarei
     // const user = await userRepository.update(request.body);
     response.status(200).json(user);
   } catch (e) {
     response.status(404).json({
-      message: "User not found",
+      message: 'User not found',
       code: 404,
     });
   }
--- user-api/src/user-repository.spec.js
+++ user-api/src/user-repository.spec.js
@@ -1,17 +1,17 @@
-const { MongoClient } = require("mongodb"); // Já importou o mongo Client
-const UserRepository = require("./user-repository");
+const {MongoClient} = require('mongodb'); // Já importou o mongo Client
+const UserRepository = require('./user-repository');
 
-describe("UserRepository", () => {
+describe('UserRepository', () => {
   let userRepository;
   let collection;
   let client;
 
   beforeAll(async () => {
-    const uri = "mongodb://localhost";
+    const uri = 'mongodb://localhost';
     // const uri = 'mongodb://rafael:123abc@localhost/usuarios?retryWrites=true&w=majority';
     client = new MongoClient(uri);
     await client.connect();
-    collection = client.db("users_db").collection("users");
+    collection = client.db('users_db').collection('users');
     userRepository = new UserRepository(collection);
   });
 
@@ -23,122 +23,122 @@ describe("UserRepository", () => {
     await collection.deleteMany({});
   });
 
-  describe("findOneById", () => {
+  describe('findOneById', () => {
     // test.todo('Deve retornar o usuário john@doe.com pelo ID');
-    test("Deve retornar o usuário john@doe.com pelo ID", async () => {
+    test('Deve retornar o usuário john@doe.com pelo ID', async () => {
       const result = await userRepository.insert({
-        name: "John Does",
-        email: "john@doe.com",
+        name: 'John Does',
+        email: 'john@doe.com',
       });
 
       const user = await userRepository.findOneById(result.id);
     });
 
     // test.todo("Deve lançar uma exceção para um usuário não existente");
-    test("Deve lançar uma exceção para um usuário não existente", async () => {
+    test('Deve lançar uma exceção para um usuário não existente', async () => {
       const user = await expect(userRepository.findOneById()).rejects.toThrow(
-        "User with email john@doe.com does not exist"
+          'User with email john@doe.com does not exist',
       );
     });
   });
 
-  describe("findOneByEmail", () => {
+  describe('findOneByEmail', () => {
     // test.todo("Deve retornar o usuário john@doe.com");
-    test("Deve retornar o usuário john@doe.com", async () => {
+    test('Deve retornar o usuário john@doe.com', async () => {
       const result = await collection.insertOne({
-        name: "John Doe",
-        email: "john@doe.com",
+        name: 'John Doe',
+        email: 'john@doe.com',
       });
 
-      const user = await userRepository.findOneByEmail("john@doe.com");
+      const user = await userRepository.findOneByEmail('john@doe.com');
       expect(user).toStrictEqual({
         id: result.insertedId,
-        name: "John Doe",
-        email: "john@doe.com",
+        name: 'John Doe',
+        email: 'john@doe.com',
       });
       // console.log(user);
     });
 
     // test.todo("Deve lançar uma exceção para um usuário não existente");
-    test("Deve lançar uma exceção para um usuário não existente", async () => {
+    test('Deve lançar uma exceção para um usuário não existente', async () => {
       await expect(
-        userRepository.findOneByEmail("john@doe.com")
-      ).rejects.toThrow("User with email john@doe.com does not exist");
+          userRepository.findOneByEmail('john@doe.com'),
+      ).rejects.toThrow('User with email john@doe.com does not exist');
     });
   });
 
-  describe("Insert", () => {
+  describe('Insert', () => {
     // test.todo("Inserir um novo usuário");
-    test("Inserir um novo usuário", async () => {
+    test('Inserir um novo usuário', async () => {
       const user = await userRepository.insert({
-        name: "John Doe",
-        email: "john@doe.com",
+        name: 'John Doe',
+        email: 'john@doe.com',
       });
-      const result = await userRepository.findOneByEmail("john@doe.com");
+      const result = await userRepository.findOneByEmail('john@doe.com');
       expect(result).toStrictEqual(user);
     });
   });
 
-  describe("update", () => {
-    test("Deve atualizar um usuário existente", async () => {
+  describe('update', () => {
+    test('Deve atualizar um usuário existente', async () => {
       const result = await userRepository.insert({
-        name: "John Doe",
-        email: "john@doe.com",
+        name: 'John Doe',
+        email: 'john@doe.com',
       });
       const user = result;
-      user.name = "Rafael";
-      user.email = "rafaelzeitoune@yahoo.com.br";
+      user.name = 'Rafael';
+      user.email = 'rafaelzeitoune@yahoo.com.br';
       expect(await userRepository.update(result.id, user)).toStrictEqual(user);
     });
     // test.todo("Deve atualizar um usuário existente" );
     // test.todo("Deve lançar uma exceção para um usuário não existente");
 
-    test("Deve lançar uma exceção para um usuário não existente para fazer update", async () => {
+    test('Deve lançar uma exceção para um usuário não existente para fazer update', async () => {
       await expect(
-        userRepository.update("61b8b326c866f10fa81cc9c2", {
+          userRepository.update('61b8b326c866f10fa81cc9c2', {
           // id: "61b8b326c866f10fa81cc9c2",
-          name: "Fernanada",
-          email: "fernanda@yahoo.com.br",
-        })
+            name: 'Fernanada',
+            email: 'fernanda@yahoo.com.br',
+          }),
       ).rejects.toThrow(
-        "Não foi possível fazer o update, o usuário não foi encontrado"
+          'Não foi possível fazer o update, o usuário não foi encontrado',
       );
     });
   });
 
-  describe("delete", () => {
+  describe('delete', () => {
     // test.todo("Deve remover um usuário existente");
-    test("Deve remover um usuário existente", async () => {
+    test('Deve remover um usuário existente', async () => {
       const user = await userRepository.insert({
-        name: "John Doe",
-        email: "john@doe.com",
+        name: 'John Doe',
+        email: 'john@doe.com',
       });
 
       await userRepository.delete(user.id);
       await expect(
-        userRepository.findOneByEmail("john@doe.com")
-      ).rejects.toThrow("User with email john@doe.com does not exist");
+          userRepository.findOneByEmail('john@doe.com'),
+      ).rejects.toThrow('User with email john@doe.com does not exist');
     });
 
-    test("Deve lançar uma exceção para um usuário não existente", async () => {
+    test('Deve lançar uma exceção para um usuário não existente', async () => {
       await expect(
-        userRepository.delete("61b8b326c866f10fa81cc9c2") // Coloquei um usuário qualquer para testar e verificar se ele irá tratar o usuário que não achar
-      ).rejects.toThrow("User with email john@doe.com does not exist");
+          userRepository.delete('61b8b326c866f10fa81cc9c2'), // Coloquei um usuário qualquer para testar e verificar se ele irá tratar o usuário que não achar
+      ).rejects.toThrow('User with email john@doe.com does not exist');
     });
     // test.todo("Deve lançar uma exceção para um usuário não existente");
   });
 
-  describe("findAll", () => {
+  describe('findAll', () => {
     // test.todo("Deve retornar uma lista vazia de usuários");
-    test("Deve retornar uma lista contendo dois usuários", async () => {
+    test('Deve retornar uma lista contendo dois usuários', async () => {
       const user = await collection.insertMany([
         {
-          name: "John Doe",
-          email: "john@doe.com",
+          name: 'John Doe',
+          email: 'john@doe.com',
         },
         {
-          name: "Kaleb Zeitoune",
-          email: "kaleb.zeitoune@yahoo.com.br",
+          name: 'Kaleb Zeitoune',
+          email: 'kaleb.zeitoune@yahoo.com.br',
         },
       ]);
       // console.log({user:{insertedIds}});
@@ -151,21 +151,21 @@ describe("UserRepository", () => {
       expect(result).toStrictEqual([
         {
           id: insertedIds[0],
-          name: "John Doe",
-          email: "john@doe.com",
+          name: 'John Doe',
+          email: 'john@doe.com',
         },
         {
           id: insertedIds[1],
-          name: "Kaleb Zeitoune",
-          email: "kaleb.zeitoune@yahoo.com.br",
+          name: 'Kaleb Zeitoune',
+          email: 'kaleb.zeitoune@yahoo.com.br',
         },
       ]);
     });
 
     // test.todo("Deve retornar uma lista vazia de usuários");
-    test("Deve retornar uma lista vazia de usuários", async () => {
+    test('Deve retornar uma lista vazia de usuários', async () => {
       await expect(userRepository.findAll()).rejects.toThrow(
-        "Não foi encontrado nenhum usuário"
+          'Não foi encontrado nenhum usuário',
       );
     });
   });
