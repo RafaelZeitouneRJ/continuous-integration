decea214d9c269344b10a5bdc1ba4c1d136d0d17
RafaelZeitouneRJ <rafaelzeitoune@yahoo.com.br>
2022-02-09 20:34:55 -0300

    Adiciona ESlint

--- user-admin/src/App.js
+++ user-admin/src/App.js
@@ -1,13 +1,16 @@
-import * as React from "react";
-import { Admin, Resource } from 'react-admin';
+import * as React from 'react';
+import {Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { UserCreate, UserEdit, UserList } from "./Users";
+import {UserCreate, UserEdit, UserList} from './Users';
 
 const dataProvider = jsonServerProvider('http://localhost:4000');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={UserList} create={UserCreate} edit={UserEdit} />
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users" list={UserList}
+      create={UserCreate}
+      edit={UserEdit} />
+  </Admin>
 );
 
 export default App;
